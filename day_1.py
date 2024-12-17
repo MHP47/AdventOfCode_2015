6bfc615b4275f6da0384b76775e8dc1935fdb380
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-17 22:54:48 +1100

    Day 1, part 2

--- day_1.py
+++ day_1.py
@@ -6,7 +6,14 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input):
-    pass
+    floor = 0
+    for i,m in enumerate(p_Input, 1):
+        floor += {
+            '(': 1,
+            ')': -1
+        }[m]
+        if floor == -1:
+            return i
 
 
 example_input_1 = "(())"
@@ -18,6 +25,8 @@ example_input_6 = "())"
 example_input_7 = "))("
 example_input_8 = ")))"
 example_input_9 = ")())())"
+example_input_10 = ")"
+example_input_11 = "()())"
 challenge_input = Input('1')
 
 assert(part_1(example_input_1) == 0)
@@ -31,5 +40,6 @@ assert(part_1(example_input_8) == -3)
 assert(part_1(example_input_9) == -3)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_10) == 1)
+assert(part_2(example_input_11) == 5)
 print(f"Part 2: {part_2(challenge_input)}")
