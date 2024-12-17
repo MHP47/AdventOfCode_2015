8c314ed470b29e28bdac2e76c8c9096c3a5a2067
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-17 23:08:25 +1100

    Day 2, part 2

--- day_2.py
+++ day_2.py
@@ -11,7 +11,12 @@ def part_1(p_Input):
 
 
 def part_2(p_Input):
-    pass
+    presents = [parse_ints(x) for x in p_Input.strip().splitlines()]
+    total = 0
+    for l,w,h in presents:
+        total += l*w*h # bow
+        total += 2*sum(sorted((l,w,h))[:2]) # around
+    return total
 
 
 example_input_1 = """2x3x4
@@ -24,5 +29,6 @@ assert(part_1(example_input_1) == 58)
 assert(part_1(example_input_2) == 43)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 34)
+assert(part_2(example_input_2) == 14)
 print(f"Part 2: {part_2(challenge_input)}")
