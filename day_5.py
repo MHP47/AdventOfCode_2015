c0da12b6ab5c93a37b7b20d6e11d0302f7f350fe
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-17 23:32:54 +1100

    Day 5, part 2

--- day_5.py
+++ day_5.py
@@ -18,7 +18,14 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input: str):
-    pass
+    total = 0
+    for word in p_Input.strip().splitlines():
+        if not re.search(r'(..).*\1', word):
+            continue
+        if not re.search(r'(.).\1', word):
+            continue
+        total += 1
+    return total
 
 
 example_input_1 = "ugknbfddgicrmopn"
@@ -26,6 +33,10 @@ example_input_2 = "aaa"
 example_input_3 = "jchzalrnumimnmhp"
 example_input_4 = "haegwjzuvuyypxyu"
 example_input_5 = "dvszwmarrgswjxmb"
+example_input_6 = "qjhvhtzxzqqjkmpb"
+example_input_7 = "xxyxx"
+example_input_8 = "uurcxstgmygtbstg"
+example_input_9 = "ieodomkazucvgmuy"
 challenge_input = Input('5')
 
 assert(part_1(example_input_1) == 1)
@@ -35,5 +46,8 @@ assert(part_1(example_input_4) == 0)
 assert(part_1(example_input_5) == 0)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_6) == 1)
+assert(part_2(example_input_7) == 1)
+assert(part_2(example_input_8) == 0)
+assert(part_2(example_input_9) == 0)
 print(f"Part 2: {part_2(challenge_input)}")
