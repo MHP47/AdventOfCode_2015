852f7fff4b732bd9e1758baef3a4571cdd4a1a30
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-17 23:23:33 +1100

    Day 4, part 2

--- day_4.py
+++ day_4.py
@@ -9,7 +9,9 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input: str):
-    pass
+    for key in itertools.count(1):
+        if hashlib.md5(f"{p_Input}{key}".encode('utf-8')).hexdigest().startswith('000000'):
+            return key
 
 
 example_input_1 = "abcdef"
@@ -20,5 +22,5 @@ assert(part_1(example_input_1) == 609043)
 assert(part_1(example_input_2) == 1048970)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+# assert(part_2(example_input_1) == None)
 print(f"Part 2: {part_2(challenge_input)}")
