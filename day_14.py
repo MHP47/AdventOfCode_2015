aab91bbacc11aee2ed825ae6d5a85107c0c6ea22
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-19 02:18:41 +1100

    Day 14, part 2

--- day_14.py
+++ day_14.py
@@ -13,8 +13,24 @@ def part_1(p_Input: str, seconds = 2503):
     )
 
 
-def part_2(p_Input: str):
-    pass
+def part_2(p_Input: str, seconds = 2503):
+    reindeer = {
+        r.split()[0]: parse_ints(r)
+        for r in p_Input.strip().splitlines()
+    }
+
+    winner = [
+        max(
+            (
+                (s*t)*(i//(t+r)) + (s*t)*min(1,(i%(t+r))/t),
+                n
+            )
+            for n,(s,t,r) in reindeer.items()
+        )[1]
+        for i in range(seconds)
+    ]
+
+    return Counter(winner).most_common(1)[0][1] - 1
 
 
 example_input_1 = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
@@ -25,5 +41,5 @@ challenge_input = Input('14')
 assert(part_1(example_input_1, 1000) == 1120)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1, 1000) == 689)
 print(f"Part 2: {part_2(challenge_input)}")
