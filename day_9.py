e3257d8cbed68e2bce60a97e9be75eeb6e105356
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-19 00:13:40 +1100

    Day 9, part 2

--- day_9.py
+++ day_9.py
@@ -24,7 +24,24 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input: str):
-    pass
+    routes = {}
+    for route in p_Input.strip().splitlines():
+        locs, dist = route.split(' = ')
+        loc_a, loc_b = locs.split(' to ')
+        dist = int(dist)
+        routes[(loc_a, loc_b)] = dist
+        routes[(loc_b, loc_a)] = dist
+
+    locations = {x for tup in routes for x in tup}
+    graph = nx.Graph(list(routes))
+    graph.add_nodes_from(locations)
+
+    return max(
+        sum(routes[k] for k in zip(path, path[1:]))
+        for start, end in itertools.permutations(locations, 2)
+        for path in nx.all_simple_paths(graph, source=start, target=end)
+        if len(path) == len(locations)
+    )
 
 
 example_input_1 = """London to Dublin = 464
@@ -36,5 +53,5 @@ challenge_input = Input('9')
 assert(part_1(example_input_1) == 605)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1) == 982)
 print(f"Part 2: {part_2(challenge_input)}")
