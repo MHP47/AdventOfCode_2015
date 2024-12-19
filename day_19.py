1949b325717d32ca0d2f39525eab1479a739f43c
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-19 22:19:28 +1100

    Day 19, part 2

--- day_19.py
+++ day_19.py
@@ -22,7 +22,24 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input: str):
-    pass
+    replacement_mappings, molecule = p_Input.strip().split('\n\n')
+    replacements = dict()
+    goal = "e"
+
+    for r in replacement_mappings.splitlines():
+        k,v = r.split(' => ')
+        replacements[v] = k
+
+    def h(s):
+        if not s: return BIG
+        return iterative_levenshtein(goal, s)
+
+    def m(s):
+        for k,v in replacements.items():
+            for i,j in [(m.start(), m.end()) for m in re.finditer(k, s)]:
+                yield f"{s[:i]}{v}{s[j:]}"
+
+    return len(astar_search(molecule, h, m)) - 1
 
 
 example_input_1 = """H => HO
@@ -37,11 +54,28 @@ O => HH
 
 HOHOHO
 """
+example_input_3 = """e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOH
+"""
+example_input_4 = """e => H
+e => O
+H => HO
+H => OH
+O => HH
+
+HOHOHO
+"""
 challenge_input = Input('19')
 
 assert(part_1(example_input_1) == 4)
 assert(part_1(example_input_2) == 7)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_3) == 3)
+assert(part_2(example_input_4) == 6)
 print(f"Part 2: {part_2(challenge_input)}")
