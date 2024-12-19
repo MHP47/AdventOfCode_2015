3e5af85d6efb78539ca05357fab2fff06610867a
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-19 23:28:41 +1100

    Day 20, part 2

--- day_20.py
+++ day_20.py
@@ -22,7 +22,23 @@ def part_1(p_Input: str):
 
 
 def part_2(p_Input: str):
-    pass
+    goal = int(p_Input)
+    elves = Counter()
+    low = 1
+    high = 1_000_000
+
+    while low <= high:
+        mid = (low + high) // 2
+        if sum(x*11 for x in divisors(mid)[:50]) >= goal:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    for i in range(1, high):
+        d = divisors(i)
+        elves.update(d)
+        if sum(x*11 for x in itertools.filterfalse(lambda y: elves[y] > 50, d)) >= goal:
+            return i
 
 
 challenge_input = '29000000'
