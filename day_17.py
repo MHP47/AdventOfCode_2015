ff19c463e14a75bc3c6a753c13f7dcb9a8ffebd2
Morgan Waldron <morgan.waldron@versent.com.au>
2024-12-19 17:21:00 +1100

    Day 17, part 2

--- day_17.py
+++ day_17.py
@@ -18,8 +18,20 @@ def part_1(p_Input: str, litres=150):
     return total
 
 
-def part_2(p_Input: str):
-    pass
+def part_2(p_Input: str, litres=150):
+    buckets = list(map(int, p_Input.strip().splitlines()))
+    matches = defaultdict(int)
+    for i in itertools.count(1):
+        combos = list(itertools.combinations(buckets, i))
+        if all(
+            sum(x) > litres
+            for x in combos
+        ):
+            break
+        for c in combos:
+            if sum(c) == litres:
+                matches[len(c)] += 1
+    return matches[min(matches)]
 
 
 example_input_1 = """20
@@ -33,5 +45,5 @@ challenge_input = Input('17')
 assert(part_1(example_input_1, 25) == 4)
 print(f"Part 1: {part_1(challenge_input)}")
 
-assert(part_2(example_input_1) == None)
+assert(part_2(example_input_1, 25) == 3)
 print(f"Part 2: {part_2(challenge_input)}")
